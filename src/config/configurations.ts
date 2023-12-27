d716d0db8e583eb6ddfbbf522fc41b456d40e501
Liza Morozova <lizavetamorozova12@gmail.com>
2023-12-27 20:59:14 +0300

    credentials are moved to a separate file

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -1,4 +1,9 @@
 export default () => ({
   secret_jwt: process.env.SECRET,
   expire_jwt: process.env.EXPIRE_JWT,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user_name: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database:  process.env.DATABASE,
 });
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        // type: 'sqlite',
-        // database: './test.sqlite',
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => ({
         synchronize: true,
         // dropSchema: true,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'endToendBackend',
-        password: '1337',
-        database: 'endToend'
+        host: configService.get('host'),
+        port: configService.get('port'),
+        username: configService.get('user_name'),
+        password: configService.get('password'),
+        database: configService.get('database'),
       }),
     }),
   ],
